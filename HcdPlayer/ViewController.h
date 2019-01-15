1be9d825058b492992b5513e61f954a3cf35b8d9
Jvaeyhcd <chedahuang@icloud.com>
2019-01-15 10:09:27 +0800

    Initial Commit

---
+++ HcdPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HcdPlayer
+//
+//  Created by Salvador on 2019/1/15.
+//  Copyright © 2019 Salvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
